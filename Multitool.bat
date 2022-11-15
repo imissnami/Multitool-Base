abf2f431043dec8227fcefb985ecc6831311bbf8
imissnami <118085775+imissnami@users.noreply.github.com>
2022-11-15 18:26:09 -0500

    Add files via upload

---
+++ Multitool.bat
@@ -0,0 +1,186 @@
+@echo off
+mode 70,0
+title Multitool
+:menu
+type main.py
+color 0D
+
+set /p action= Enter Option Here:
+if '%action%'=='1' goto 
+if '%action%'=='2' goto 
+if '%action%'=='3' goto 
+if '%action%'=='4' goto 
+if '%action%'=='5' goto 
+if '%action%'=='6' goto 
+if '%action%'=='7' goto 
+if '%action%'=='8' goto 
+if '%action%'=='9' goto 
+if '%action%'=='10' goto 
+if '%action%'=='11' goto 
+if '%action%'=='12' goto 
+if '%action%'=='13' goto 
+if '%action%'=='14' goto 
+if '%action%'=='15' goto 
+if '%action%'=='16' goto 
+if '%action%'=='17' goto 
+if '%action%'=='18' goto 
+if '%action%'=='19' goto 
+if '%action%'=='20' goto 
+if '%action%'=='21' goto 
+if '%action%'=='22' goto  
+if '%action%'=='23' goto 
+if '%action%'=='24' goto 
+if '%action%'=='25' goto 
+if '%action%'=='26' goto 
+if '%action%'=='27' goto 
+if '%action%'=='28' goto 
+if '%action%'=='29' goto 
+if '%action%'=='30' goto exit
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\ 
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:
+@echo off &  cls
+start %cd%\PROGRAMS\
+goto menu
+
+:Monotone 
+@echo off & cls
+explorer 
+goto menu
+
+:
+@echo off & cls
+explorer 
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:
+@echo off & cls
+explorer 
+goto menu
+
+:
+@echo off & cls
+explorer 
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:
+@echo off & cls
+explorer 
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:
+@echo off & cls
+explorer 
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:
+@echo off & cls
+explorer 
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:
+@echo off & cls
+explorer 
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:
+@echo off & cls
+start %cd%\PROGRAMS\
+goto menu
+
+:exit
+quit
